3ec2a59bed214a0694f6ee251d66eb35ca587227
yunkwan.kim <yunkwan.kim@gmail.com>
2015-06-12 04:07:20 +0900

    add BOARDCOVER
    https://algospot.com/judge/problem/read/BOARDCOVER
    
    fix problems and report complete.
    last code using One-dimensional arrays has a problem for no outline of
    board.
    it could not classify left or right boundary.

--- src/com/yunkwan/exam/BOARDCOVER.java
+++ src/com/yunkwan/exam/BOARDCOVER.java
@@ -3,8 +3,6 @@ package com.yunkwan.exam;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BOARDCOVER {
@@ -12,7 +10,6 @@ public class BOARDCOVER {
 		BOARDCOVER m = new BOARDCOVER();
 		m.start();
 	}
-
 	
 	private int h;
 	private int w;
@@ -21,7 +18,8 @@ public class BOARDCOVER {
 	 * block[0] = ##, block[1]  #, block[2] ##, block[3] #
 	 *            #            ##            #           ##
 	 */
-	private int[][] blocks = null;
+	private int[][] blocksH = new int[][]{{0, 0, 1},{0, 1, 1},{0, 0, 1},{0, 1, 1}};
+	private int[][] blocksW = new int[][]{{0, 1, 0},{0, -1, 0},{0, 1, 1},{0, 0, 1}};
 	
 	public void start() throws FileNotFoundException {
 		// Scanner sc = new Scanner(System.in);
@@ -33,11 +31,9 @@ public class BOARDCOVER {
 			h = sc.nextInt();
 			w = sc.nextInt();
 		
-			blocks = new int[][]{{0, 1, w},{0, w-1, w},{0, 1, w+1},{0, w, w+1}};
-			
-			String map = "";
+			char[][] map = new char[h][];
 			for (int i = 0; i < h; i++) {
-				map += sc.next();
+				map[i] = sc.next().toCharArray();
 			}
 			if(checkValid(map)) {
 				System.out.println(getSuccessfulTryCount(map));
@@ -46,49 +42,70 @@ public class BOARDCOVER {
 			}
 		}
 	}
-	private boolean checkValid(String map2) {
+	private boolean checkValid(char[][] map2) {
 		int validBlock = 0;
-		char[] tmp = map2.toCharArray();
-		for (int j = map2.length()-1; j >= 0 ; j--) {
-			if(tmp[j] == '.') {
-				validBlock++;
+		for (int i = h-1; i >= 0 ; i--) {
+			for (int j = w-1; j >= 0 ; j--) {
+				if(map2[i][j] == '.') {
+					validBlock++;
+				}
 			}
 		}
 		return (validBlock%3 == 0)? true:false;
 	}
-	public int getSuccessfulTryCount(String map) {
+	public int getSuccessfulTryCount(char[][] map) {
 		int max = 0;
-		int startLoc = map.indexOf(".");
-		if(startLoc == -1 ) {
+		int startLocH = -1, startLocW = -1;
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w ; j++) {
+				if(map[i][j] == '.') {
+					startLocH = i;
+					startLocW = j;
+					i=h;
+					break;
+				}
+			}
+		}
+		if(startLocH < 0 ) {
 			return 1;
 		}
 		for(int i=0;i<4;i++) {
-			String ret = check(i, startLoc, map);
+			char [][] ret = check(i, startLocH, startLocW, map);
 			if( ret != null) {
+				//print(ret);
 				max += getSuccessfulTryCount(ret);
 			}
 		}
 		return max;
 	}
 	
-	public String check(int block, int index, String map) {
-		char[] tmp = map.toCharArray();
-		for (int j = 0; j < 3; j++) {
-			int loc = index + blocks[block][j];
-			if( loc > h*w) {
-				return null;
-			}
-			if(tmp[loc] == '#') {
-				return null;
-			}
-			tmp[loc] = '#';
+	public char[][] check(int block, int indexH, int indexW, char[][] map) {
+		// performance improvement points
+		char[][] tmp = new char[h][];
+		for (int j = 0; j < h; j++) {
+			tmp[j] = map[j].clone();
 		}
-		return String.valueOf(tmp);
+		
+		for (int j = 0; j < 3; j++) {
+			int locH = indexH + blocksH[block][j];
+			int locW = indexW + blocksW[block][j];
+			if( locH >= h || locH < 0 || locW >= w || locW < 0) {
+				return null;
+			}
+			if(tmp[locH][locW] == '#') {
+				return null;
+			}
+			tmp[locH][locW] = '#';
+		}
+		return tmp;
 	}
 	
-	public void print(String map) {
+	public void print(char[][] map) {
 		for (int i = 0; i < h; i++) {
-			System.out.println(map.substring(i*w, (i*w)+w));
+			for (int j = 0; j < w; j++) {
+				System.out.print(map[i][j]);
+			}
+			System.out.println();
 		}
 		System.out.println();
 	}
