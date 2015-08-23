ba859cf82ed2fe44984d1033af5ad86e9a8a8dbf
yunkwan.kim <yunkwan.kim@gmail.com>
2015-08-23 14:53:24 +0900

    Astro-rescue
    Problem 4 in LGE Codejam
    https://codejam.lge.com/contest/index

---
+++ src/com/yunkwan/exam/Problem4_astrorescue.java
@@ -0,0 +1,71 @@
+package com.yunkwan.exam;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Problem4_astrorescue {
+	
+    public static void main(String args[]) throws FileNotFoundException {
+        Scanner sc = new Scanner(new BufferedInputStream(new FileInputStream("Problem4.txt")));
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int N = sc.nextInt()-1;
+            int K = sc.nextInt();
+            for (int i = 0; i < N; i++) {
+            	edges.add(sc.nextInt());
+            	edges.add(sc.nextInt());
+            }
+            for (int i = 0; i < K; i++) {
+                System.out.println(calculate(N, sc.nextInt(), sc.nextInt()));
+            }
+            edges.clear();
+        }
+    }
+    
+    public static ArrayList<Integer> edges = new ArrayList<Integer>(); 
+    
+    public static ArrayList<Integer> findPath(int n, ArrayList<Integer> path, int current, int l) {
+    	ArrayList<Integer> ret = null;
+		path.add(current);
+    	for (int i = 0; i < n; i++) {
+    		int a = edges.get(i*2);
+    		int b = edges.get(i*2+1);
+    		if(a == current && path.indexOf(b) < 0) {
+    			if(l == b) {
+    				path.add(l);
+    				return path; // found
+    			}
+    			ret = findPath(n, path, b, l);
+    		}
+    		if(b == current && path.indexOf(a) < 0) {
+    			if(l == a) {
+    				path.add(l);
+    				return path; // found
+    			}
+    			ret = findPath(n, path, a, l);
+    		}
+    		if(ret != null) return ret;
+    	}
+    	path.remove(path.size()-1);
+    	return ret;
+    }
+    
+    public static int calculate(int n, int a, int b) {
+    	ArrayList<Integer> path = new ArrayList<Integer>();
+    	path = findPath(n, path, a, b);
+    	print(path);
+    	int ret = (path.size()+1)/2-1;
+    	return path.get(ret);
+    }
+    
+    public static void print(ArrayList<Integer> path) {
+    	for(int i=0;i<path.size();i++) {
+    		System.out.print(path.get(i) + " ");
+    	}
+    	System.out.println(":" + path.size());
+    }
+
+}
