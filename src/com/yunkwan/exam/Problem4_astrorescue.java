471a379657d69430927c1075a1ae2215ce48fd20
yunkwan.kim <yunkwan.kim@gmail.com>
2015-08-24 02:20:41 +0900

    performance problem,
    still slow

--- src/com/yunkwan/exam/Problem4_astrorescue.java
+++ src/com/yunkwan/exam/Problem4_astrorescue.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Problem4_astrorescue {
 	
     public static void main(String args[]) throws FileNotFoundException {
-        Scanner sc = new Scanner(new BufferedInputStream(new FileInputStream("Problem4.txt")));
+        Scanner sc = new Scanner(new BufferedInputStream(new FileInputStream("problem4.in")));
         int tc = sc.nextInt();
         while (tc-- > 0) {
             int N = sc.nextInt()-1;
@@ -29,34 +29,34 @@ public class Problem4_astrorescue {
     
     public static ArrayList<Integer> findPath(int n, ArrayList<Integer> path, int current, int l) {
     	ArrayList<Integer> ret = null;
-		path.add(current);
+		path.add(0, current);
     	for (int i = 0; i < n; i++) {
     		int a = edges.get(i*2);
     		int b = edges.get(i*2+1);
-    		if(a == current && path.indexOf(b) < 0) {
+    		if(a == current && !path.contains(b)) {
     			if(l == b) {
-    				path.add(l);
+    				path.add(0, l);
     				return path; // found
     			}
     			ret = findPath(n, path, b, l);
     		}
-    		if(b == current && path.indexOf(a) < 0) {
+    		else if(b == current && !path.contains(a)) {
     			if(l == a) {
-    				path.add(l);
+    				path.add(0, l);
     				return path; // found
     			}
     			ret = findPath(n, path, a, l);
     		}
     		if(ret != null) return ret;
     	}
-    	path.remove(path.size()-1);
+    	path.remove(0);
     	return ret;
     }
     
     public static int calculate(int n, int a, int b) {
     	ArrayList<Integer> path = new ArrayList<Integer>();
     	path = findPath(n, path, a, b);
-    	print(path);
+    	//print(path);
     	int ret = (path.size()+1)/2-1;
     	return path.get(ret);
     }
